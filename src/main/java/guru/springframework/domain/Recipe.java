c86e7489a3f983c1ab9a8c39ff6fde1dce2319ea
KazChe <kaz.che@gmail.com>
2020-09-19 10:53:22 -0700

    OneToOne JPA Relationships - Recipe and Notes. Closes #1

---
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -0,0 +1,109 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+    private String description;
+    private String prepTime;
+    private String cooktime;
+    private Integer serving;
+    private String source;
+    private String url;
+    private String directions;
+
+    @Lob
+    private Byte[] image;
+
+    //todo add
+    // private Difficulty difficulty;
+
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPrepTime() {
+        return prepTime;
+    }
+
+    public void setPrepTime(String prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public String getCooktime() {
+        return cooktime;
+    }
+
+    public void setCooktime(String cooktime) {
+        this.cooktime = cooktime;
+    }
+
+    public Integer getServing() {
+        return serving;
+    }
+
+    public void setServing(Integer serving) {
+        this.serving = serving;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+}
