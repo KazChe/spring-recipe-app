8d2362d747d389d0a2edc71b81f6a9459cb0cf63
KazChe <kaz.che@gmail.com>
2020-09-25 15:51:22 -0700

    cleaned index controller from unnecessary objects.

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -10,13 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    private final CategoryRepository categoryRepository;
-    private final UnitOfMeasureRepository unitOfMeasureReposiry;
     private final RecipeService recipeService;
 
     public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository, RecipeService recipeService) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureReposiry = unitOfMeasureRepository;
         this.recipeService = recipeService;
     }
 
